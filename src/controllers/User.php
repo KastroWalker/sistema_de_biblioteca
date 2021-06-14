4351063dff65b118b4232e6bc870237fc8c39405
Kauan Portela <kauanphbbb@gmail.com>
2021-06-13 21:38:17 -0300

    CRUD of users

--- src/controllers/User.php
+++ src/controllers/User.php
@@ -1,11 +1,13 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/src/models/LoginModel.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/models/UserModel.php';
 
 class User
 {
     public function __construct()
     {
         $this->loginModel = new LoginModel();
+        $this->userModel = new UserModel();
     }
 
     public function login($request)
@@ -25,5 +27,64 @@ class User
 
             return $response;
         }
+    }
+
+    public function getAll()
+    {
+        $response = [
+            'status' => 'error',
+            'message' => 'Erro ao procurar livros'
+        ];
+
+        $user = $this->userModel->getAll();
+
+        if ($user) {
+            $response = [
+                'status' => 'success',
+                'user' => $user
+            ];
+        }
+
+        return $response;
+    }
+
+    public function getById($id)
+    {
+        $reponse = [
+            'status' => 'error',
+            'message' => 'Erro ao pegar dados do livro'
+        ];
+
+        $filter = ['id' => $id];
+
+        $user = $this->userModel->getBy($filter);
+
+        if ($user) {
+            $reponse = [
+                'status' => 'success',
+                'user' => $user[0]
+            ];
+        }
+
+        return $reponse;
+    }
+
+    public function create($request)
+    {
+        $data = filter_var_array($request, FILTER_SANITIZE_STRIPPED);
+
+        $this->userModel->store($data);
+    }
+
+    public function update($request, $filter)
+    {
+        $data = filter_var_array($request, FILTER_SANITIZE_STRIPPED);
+
+        $this->userModel->update($data, $filter);
+    }
+
+    public function delete($filter)
+    {
+        $this->userModel->delete($filter);
     }
 }
---
+++ src/models/UserModel.php
@@ -0,0 +1,128 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/models/Model.php';
+
+class UserModel extends Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->table = "user";
+    }
+
+    public function getAll()
+    {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM {$this->table};");
+
+            $stmt->execute();
+
+            return $stmt->fetchAll();
+        } catch (Exception $ex) {
+            Log::createLog($ex);
+        }
+    }
+
+    public function getBy(array $filter = [], string $filterString = "")
+    {
+        $fields = '';
+        $bind = [];
+        $counter = 1;
+        $lengthFilter = count($filter);
+
+        if ($filterString) {
+            $stmt = $this->connection->prepare("SELECT * FROM {$this->table} WHERE $filterString;");
+
+            $stmt->execute();
+        } else {
+            foreach ($filter as $key => $value) {
+                $fields .= "$key = :$key";
+
+                if ($counter < $lengthFilter) {
+                    $fields .= " AND ";
+                }
+
+                $counter++;
+                $bind[":$key"] = $value;
+            }
+
+            $stmt = $this->connection->prepare("SELECT * FROM {$this->table} WHERE $fields;");
+
+            $stmt->execute($bind);
+        }
+
+        return $stmt->fetchAll();
+    }
+
+    public function store(array $data)
+    {
+        try {
+            $keys = array_keys($data);
+            $values = ':' . implode(',:', $keys);
+            $fields = implode(",", $keys);
+
+            $stmt = $this->connection->prepare("INSERT INTO {$this->table} ({$fields}) VALUES ({$values})");
+
+            $stmt->execute($data);
+        } catch (Exception $ex) {
+            Log::createLog($ex);
+        }
+    }
+
+    public function update(array $data, $filter = null)
+    {
+        try {
+            $fileds = '';
+            $filterString = '';
+            $bind = [];
+
+            if (is_array($filter)) {
+                foreach ($filter as $key => $value) {
+                    $filterString = "{$key} = :{$key} ";
+                    $bind[":$key"] = $value;
+                }
+            } else if (is_array($filter)) {
+                $filterString = $filter;
+            } else {
+                throw new Exception("Filter is invalid", 1);
+            }
+
+            foreach ($data as $key => $value) {
+                $fileds .= "{$key} = :{$key}, ";
+                $bind[":$key"] = $value;
+            }
+
+            $fileds = substr($fileds, 0, -2);
+
+            $stmt = $this->connection->prepare("UPDATE {$this->table} SET {$fileds} WHERE $filterString");
+
+            $stmt->execute($bind);
+        } catch (Exception $ex) {
+            Log::createLog($ex);
+        }
+    }
+
+    public function delete($filter)
+    {
+        try {
+            $filterString = '';
+            $bind = [];
+
+            if (is_array($filter)) {
+                foreach ($filter as $key => $value) {
+                    $filterString = "{$key} = :{$key} ";
+                    $bind[":$key"] = $value;
+                }
+            } else if (is_string($filter)) {
+                $filterString = $filter;
+            } else {
+                throw new Exception("Filter is invalid", 1);
+            }
+
+            $stmt = $this->connection->prepare("DELETE FROM {$this->table} WHERE $filterString");
+
+            $stmt->execute($bind);
+        } catch (Exception $ex) {
+            Log::createLog($ex);
+        }
+    }
+}
